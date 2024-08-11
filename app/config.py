863a532585dbb6c53881b46cbeb3560fcb42e919
Sam <162775049+Samwong077@users.noreply.github.com>
2024-08-11 14:41:58 +1000

    Add files via upload

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://fuksangwong_sit722_part1_user:rvW45XnNP5z1h5G7wmzjcuxbRBDB6Wa8@dpg-cqs3j1ggph6c73a6uaqg-a.oregon-postgres.render.com/fuksangwong_sit722_part1")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
